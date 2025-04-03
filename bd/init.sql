61d5914b718c50fa40ab461819a89fb0655ffb0f
Nathan Royer <nathan.royer@etu.univ-lehavre.fr>
2025-04-03 14:49:11 +0200

    ajout de produits dans la bd

--- bd/init.sql
+++ bd/init.sql
@@ -161,6 +161,9 @@ INSERT INTO Produits (nom_prod, description, prix, stock, category) VALUES
 ('T-shirt', 'T-shirt en coton bio', 15.00, 100, 'Vetements'),
 ('Mug', 'Mug en céramique', 10.00, 50, 'Goodies'),
 ('Sweatshirt', 'Sweatshirt en polaire', 30.00, 75, 'Vetements');
+('Casquette', 'Casquette en tissu respirant', 12.00, 60, 'Accessoires'),
+('Sac à dos', 'Sac à dos en toile robuste', 40.00, 30, 'Bagagerie'),
+('Bouteille Isotherme', 'Bouteille en acier inoxydable', 20.00, 80, 'Goodies');
 
 INSERT INTO Contact (id_utilisateur, sujet, contenu) VALUES
 (1, 'Problème de connexion', 'Je n’arrive pas à me connecter à mon compte. Pouvez-vous m’aider ?'),
